b57317505f4692c17616e24bbf7c90cb383e5be6
naomi <naomi.emma.spencer@gmail.com>
2016-08-05 16:41:51 +0100

    center

--- directory.rb
+++ directory.rb
@@ -3,24 +3,25 @@ student = {name: "Naomi Spencer", cohort: :november,
     age: 27, country: "Britian", height: "5ft"}
 
 def print_header
-  puts "Please see below profile of a student of villains Academy"
-  puts "____________"
+  puts "Please see below profile of a student of villains Academy".center(90)
+  puts "____________".center(90)
 end
 
 #define printing names method
 
 def print_students(student)
     puts "(This student is called #{student[:name]})
-    (They are from the #{student[:cohort]} cohort)
-    (This student is #{student[:age]} years old)
-    (Their country of birth is #{student[:country]})
-    (Their height is #{student[:height]})"
+          (They are from the #{student[:cohort]} cohort)
+          (This student is #{student[:age]} years old)
+          (Their country of birth is #{student[:country]})
+          (Their height is #{student[:height]})".lines.map { |line| line.strip.center(90) }.join("\n")
 end
 
 def print_footer
-  puts "____________"
-  puts "End of student profile"
+  puts "____________".center(90)
+  puts "End of student profile".center(90)
 end
+
 # nothing happens until we call the methods
 student
 print_header
