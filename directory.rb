e828733d3d2821ce31af8d0ac7832d749fe1baa0
naomi <naomi.emma.spencer@gmail.com>
2016-08-07 21:46:44 +0100

    string to symbol and two user inputs

--- directory.rb
+++ directory.rb
@@ -1,29 +1,24 @@
+def input_students
+# students name
+ puts "Please enter the names of the students"
+#create an empty array
+ students = []
+# get the first name
+ name = gets.chomp.to_sym
+ name = nil
 
-student = {name: "Naomi Spencer", cohort: :november,
-    age: 27, country: "Britian", height: "5ft"}
-
-def print_header
-  puts "Please see below profile of a student of villains Academy".center(90)
-  puts "____________".center(90)
+def student_cohort
+#create an empty array
+ cohort = []
+ month = nil
+ until (month == "January" or month == "September")
+    puts "Please enter January or September."
+    month = gets.chomp
 end
-
-#define printing names method
-
-def print_students(student)
-    puts "(This student is called #{student[:name]})
-          (They are from the #{student[:cohort]} cohort)
-          (This student is #{student[:age]} years old)
-          (Their country of birth is #{student[:country]})
-          (Their height is #{student[:height]})".lines.map { |line| line.strip.center(90) }.join("\n")
 end
-
-def print_footer
-  puts "____________".center(90)
-  puts "End of student profile".center(90)
 end
 
 # nothing happens until we call the methods
-student
-print_header
-print_students(student)
-print_footer
+# nothing happens until we call the methods
+students = input_students
+cohort = student_cohort
