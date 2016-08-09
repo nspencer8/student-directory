90328e0c917ef69e96f5930bfec195cb7d103da5
naomi <naomi.emma.spencer@gmail.com>
2016-08-09 11:43:11 +0100

    convert to sym

--- directory.rb
+++ directory.rb
@@ -3,22 +3,46 @@ def input_students
  puts "Please enter the names of the students"
 #create an empty array
  students = []
-# get the first name
- name = gets.chomp.to_sym
- name = nil
+  # get the first name
+  name = gets.chomp
 
-def student_cohort
-#create an empty array
- cohort = []
- month = nil
- until (month == "January" or month == "September")
-    puts "Please enter January or September."
+
+  while !name.empty? do
+    puts "Please enter the students cohort"
+    # get the first name
     month = gets.chomp
+    until ["January",
+                  "February",
+                  "March",
+                  "April",
+                  "May",
+                  "June",
+                  "July",
+                  "August",
+                  "September",
+                  "October",
+                  "November",
+                  "December"
+      ].include?(month)
+      puts "Please put correct month"
+      month = gets.chomp
+    end
+    # while the name is not empty, repeat this code
+      if month == ""
+        month = "January"
+      end
+  # add the student hash to the array
+      students << {name: name, cohort: month.to_sym}
+      puts "Now we have #{students.count} students"
+      # get another name from the user
+      name = gets.chomp
+
+  end
+  students
 end
-end
-end
+
+
 
 # nothing happens until we call the methods
 # nothing happens until we call the methods
-students = input_students
-cohort = student_cohort
+puts students = input_students
