301ad2b81d0c80ba4ccbd0def3a58f65a7f6322f
naomi <naomi.emma.spencer@gmail.com>
2016-08-09 12:22:21 +0100

    group print

--- directory.rb
+++ directory.rb
@@ -41,8 +41,26 @@ def input_students
   students
 end
 
+def print_header
+  puts "Please see below profile of a student of villains Academy".center(90)
+  puts "____________".center(90)
+end
+
+#define printing names method
+
+def print_students(student)
+    puts student.group_by { |entry| entry[:cohort] }
+end
+
+def print_footer
+  puts "____________".center(90)
+  puts "End of student profile".center(90)
+end
+
+
 
 
 # nothing happens until we call the methods
 # nothing happens until we call the methods
-puts students = input_students
+students = input_students
+print_students(students)
