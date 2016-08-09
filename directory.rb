d97bc300ee87319be019978b9e532916178282d9
naomi <naomi.emma.spencer@gmail.com>
2016-08-09 16:14:35 +0100

    no input print

--- directory.rb
+++ directory.rb
@@ -2,7 +2,7 @@ def input_students
 # students name
  puts "Please enter the names of the students"
 #create an empty array
- students = []
+ @students = []
   # get the first name
   name = gets.strip
 
@@ -31,8 +31,8 @@ while !name.empty? do
       month = "January"
     end
   # add the student hash to the array
-    students << {name: name, cohort: month.to_sym}
-    if students.count == 1
+    @students << {name: name, cohort: month.to_sym}
+    if @students.count == 1
       puts "Now we have #{students.count} student"
     else
       puts "Now we have #{students.count} students"
@@ -40,26 +40,38 @@ while !name.empty? do
     end
   name = gets.strip
   end
-  students
+  @students
 end
 
-def print_header
-  puts "Please see below profile of a student of villains Academy".center(90)
-  puts "____________".center(90)
-end
 
-#define printing names method
+  def print_header
+    puts "Please see below profile of a student of villains Academy".center(90)
+    puts "____________".center(90)
+  end
 
-def print_students(student)
-    puts student.group_by { |entry| entry[:cohort] }
-end
+  #define printing names method
 
-def print_footer
-  puts "____________".center(90)
-  puts "End of student profile".center(90)
-end
+  def print_students(student)
+      puts student.group_by { |entry| entry[:cohort] }
+  end
+
+  def print_footer
+    puts "____________".center(90)
+    puts "End of student profile".center(90)
+  end
+
+
+
+
 
 # nothing happens until we call the methods
 # nothing happens until we call the methods
 students = input_students
+if @students != []
 print_students(students)
+print_header
+print(students)
+print_footer
+else
+  puts "no student records"
+end
