17a06f9d216da52db87aeed271c459a9bc344d6e
naomi <naomi.emma.spencer@gmail.com>
2016-08-11 13:45:37 +0100

    interactive menu

--- directory.rb
+++ directory.rb
@@ -2,7 +2,7 @@ def input_students
 # students name
  puts "Please enter the names of the students"
 #create an empty array
- @students = []
+ students = []
   # get the first name
   name = gets.strip
 
@@ -31,8 +31,8 @@ while !name.empty? do
       month = "January"
     end
   # add the student hash to the array
-    @students << {name: name, cohort: month.to_sym}
-    if @students.count == 1
+    students << {name: name, cohort: month.to_sym}
+    if students.count == 1
       puts "Now we have #{students.count} student"
     else
       puts "Now we have #{students.count} students"
@@ -40,9 +40,35 @@ while !name.empty? do
     end
   name = gets.strip
   end
-  @students
+  students
 end
 
+def interactive_menu
+   students = []
+  loop do
+    # 1. print the menu and ask the user what to do
+    puts "1. Input the students"
+    puts "2. Show the Students"
+    puts "9. Exit" # 9 because we'll be adding more items
+    # 2. read the input and save it into a variable
+    selection = gets.chomp
+    # 3. do what the user has asked
+    case selection
+    when "1"
+      #input the students
+      students = input_students
+    when "2"
+      # show the students
+      print_header
+      print(students)
+      print_footer
+    when "9"
+      exit # this will casue the program to terminate
+    else
+      puts "I don't know what you meant, try again"
+    end
+end
+end
 
   def print_header
     puts "Please see below profile of a student of villains Academy".center(90)
@@ -60,18 +86,4 @@ end
     puts "End of student profile".center(90)
   end
 
-
-
-
-
-# nothing happens until we call the methods
-# nothing happens until we call the methods
-students = input_students
-if @students != []
-print_students(students)
-print_header
-print(students)
-print_footer
-else
-  puts "no student records"
-end
+students = interactive_menu
